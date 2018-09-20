7b6b20cb9df682e640c0869418cf218c62befb72
mzramna <miguelzinhorama@gmail.com>
2018-09-20 19:29:27 -0300

    Update gdriveeasydownloader.sh
    implemented the wget option -q --show-progress

--- gdriveeasydownloader.sh
+++ gdriveeasydownloader.sh
@@ -2,6 +2,6 @@
 echo "insert the google drive full url to download";
 read FILEID;
 FILEID="$(echo $FILEID | sed -n 's#.*\https\:\/\/drive\.google\.com/file/d/\([^.]*\)\/view.*#\1#;p')";
-FILENAME="$(wget --quiet -O - "https://drive.google.com/file/d/$FILEID/view" | sed -n -e 's!.*<title>\(.*\)\ \-\ Google\ Drive</title>.*!\1!p')";
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$FILEID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -c -O $FILENAME && rm -rf /tmp/cookies.txt;
+FILENAME="$(wget -q --show-progress -O - "https://drive.google.com/file/d/$FILEID/view" | sed -n -e 's!.*<title>\(.*\)\ \-\ Google\ Drive</title>.*!\1!p')";
+wget -q --show-progress --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget -q --show-progress --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$FILEID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -c -O $FILENAME && rm -rf /tmp/cookies.txt;
 echo "file $FILENAME has been downloaded"
