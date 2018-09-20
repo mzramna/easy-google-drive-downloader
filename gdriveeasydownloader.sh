7080d57c8abcde00aef9fd4f7805e7369551fbfb
mzramna <miguelzinhorama@gmail.com>
2018-09-19 23:13:28 -0300

    Create gdriveeasydownloader.sh
    this is the first step to the algorithim for better google drive downloading from terminal

---
+++ gdriveeasydownloader.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "insert the google drive full url to download";
+read FILEID;
+FILEID="$(echo $FILEID | sed -n 's#.*\https\:\/\/drive\.google\.com/file/d/\([^.]*\)\/view.*#\1#;p')";
+FILENAME="$(wget --quiet -O - "https://drive.google.com/file/d/$FILEID/view" | sed -n -e 's!.*<title>\(.*\)\ \-\ Google\ Drive</title>.*!\1!p')";
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$FILEID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -c -O $FILENAME && rm -rf /tmp/cookies.txt;
+echo "file $FILENAME has been downloaded"
