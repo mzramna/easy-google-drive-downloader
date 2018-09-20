005eb65e0bf9d8a496dd8a910801bf0930805005
mzramna <miguelzinhorama@gmail.com>
2018-09-20 12:34:41 -0300

    Update download_google_docs_file.sh
    auto get file name implemented

--- download_google_docs_file.sh
+++ download_google_docs_file.sh
@@ -3,8 +3,7 @@ echo "insert the google service name (in english) to download (if you write it w
 read GOOGLE_SERVICE;
 echo "insert the google $GOOGLE_SERVICE ID to download";
 read FILEID;
-echo "insert the google $GOOGLE_SERVICE NAME to download";
-read FILENAME;
+FILENAME="$(wget --quiet -O - "https://drive.google.com/file/d/$FILEID/view" | sed -n -e 's!.*<title>\(.*\)\ \-\ Google\ Drive</title>.*!\1!p')";
 echo "insert the google $GOOGLE_SERVICE EXTENSION to download(if you insert a wrong or not suportated file extension it may not work)";
 read FILEEXTENSION;
 wget -c "https://docs.google.com/$GOOGLE_SERVICE/export?format=$FILEEXTENSION&id=$FILEID&includes_info_params=true" -O "$FILENAME.$FILEEXTENSION"
