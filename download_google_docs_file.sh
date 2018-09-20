fd3777f54dc2f2b5af974c28386c099ba12eaf74
mzramna <miguelzinhorama@gmail.com>
2018-09-20 18:10:14 -0300

    Update download_google_docs_file.sh
    now it just have to insert the brute url to download the gdocs file

--- download_google_docs_file.sh
+++ download_google_docs_file.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "insert the google service name (in english) to download (if you write it wrong the code will not work)";
-read GOOGLE_SERVICE;
-echo "insert the google $GOOGLE_SERVICE ID to download";
-read FILEID;
+echo "insert the google doc url";
+read FILEURL;
+GOOGLE_SERVICEFILEID="$(echo $FILEURL | sed -n 's#.*\https\:\/\/docs\.google\.com\/\([^.]*\)\/d\/*.*#\1#;p')";
+FILEID="$(echo $FILEURL | sed -n "s#.*\https\:\/\/docs\.google\.com\/$GOOGLE_SERVICEFILEID\/d\/\([^.]*\)\/.*#\1#;p")";
 FILENAME="$(wget --quiet -O - "https://drive.google.com/file/d/$FILEID/view" | sed -n -e 's!.*<title>\(.*\)\ \-\ Google\ Drive</title>.*!\1!p')";
 echo "insert the google $GOOGLE_SERVICE EXTENSION to download(if you insert a wrong or not suportated file extension it may not work)";
 read FILEEXTENSION;
-wget -c "https://docs.google.com/$GOOGLE_SERVICE/export?format=$FILEEXTENSION&id=$FILEID&includes_info_params=true" -O "$FILENAME.$FILEEXTENSION"
+wget -c "https://docs.google.com/$GOOGLE_SERVICEexport?format=$FILEEXTENSION&id=$FILEID&includes_info_params=true" -O "$FILENAME.$FILEEXTENSION"
