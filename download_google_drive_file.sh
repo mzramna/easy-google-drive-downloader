df1b96dafebf49bc447146d04c90690890ba4865
mzramna <miguelzinhorama@gmail.com>
2019-01-14 08:31:54 -0200

    addeded parameter
    now if it had a parameter it will download without ask to insert this manualy in terminal

--- download_google_drive_file.sh
+++ download_google_drive_file.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo "insert the google drive full url to download";
-read FILEID;
+if [ $# -eq 1 ];then
+	FILEID=$1	
+else
+	echo "insert the google drive full url to download";
+	read FILEID;
+fi
 FILEID="$(echo $FILEID | sed -n 's#.*\https\:\/\/drive\.google\.com/file/d/\([^.]*\)\/view.*#\1#;p')";
 FILENAME="$(wget -q --show-progress -O - "https://drive.google.com/file/d/$FILEID/view" | sed -n -e 's!.*<title>\(.*\)\ \-\ Google\ Drive</title>.*!\1!p')";
 wget -q --show-progress --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget -q --show-progress --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$FILEID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -c -O "$FILENAME" && rm -rf /tmp/cookies.txt;
