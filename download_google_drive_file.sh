048ade5edd63898c37c36a59e5b3b4b0020b7c83
Miguel Magalhães Lopes <miguelzinhorama@gmail.com>
2020-10-20 12:38:03 -0300

    improved message system
    bug detected: anyhow cannot solve drive.google.com host with wget

--- download_google_drive_file.sh
+++ download_google_drive_file.sh
@@ -9,4 +9,8 @@ FILEID="$(echo $FILEID | sed -n 's#.*\https\:\/\/drive\.google\.com/file/d/\([^.
 FILENAME="$(wget -q --show-progress -O - "https://drive.google.com/file/d/$FILEID/view" | sed -n -e 's!.*<title>\(.*\)\ \-\ Google\ Drive</title>.*!\1!p')";
 CONFIRM_CODE=$(wget -q --show-progress --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$FILEID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
 wget -q --show-progress --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM_CODE&id=$FILEID" -c -O "$FILENAME" && rm -rf /tmp/cookies.txt;
-echo "file $FILENAME has been downloaded"
+if [ -f "$FILENAME" ]; then
+	echo "file $FILENAME has been downloaded"
+else
+	echo "file $FILENAME cannot be downloaded,any error appear"
+fi
