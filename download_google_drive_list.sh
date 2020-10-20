4a13ffeb4db30064a6a2301234131b7edc71ec7e
Miguel Magalhães Lopes <miguelzinhorama@gmail.com>
2020-10-20 12:39:17 -0300

    created logfile

--- download_google_drive_list.sh
+++ download_google_drive_list.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 filename='gdrive_links.txt'
+log="download_log.log"
 echo Start
 #
 #pidlist=()
@@ -13,7 +14,11 @@ echo $FILEID
 FILENAME="$(wget -q --show-progress -O - "https://drive.google.com/file/d/$FILEID/view" | sed -n -e 's!.*<title>\(.*\)\ \-\ Google\ Drive</title>.*!\1!p')";
 CONFIRM_CODE=$(wget -q --show-progress --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$FILEID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
 wget -q --show-progress --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM_CODE&id=$FILEID" -c -O "$FILENAME" && rm -rf /tmp/cookies.txt;
-echo "file $FILENAME has been downloaded"
+if [ -f "$FILENAME" ]; then
+	echo "file $FILENAME has been downloaded" >> $log
+else
+	echo "file $FILENAME cannot be downloaded,any error appear" >> $log
+fi
 
 }
 
